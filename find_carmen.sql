0d36e7fe7119217067e0267c0e1e416359326b9a
Zahralsayed <zalmatooq31@gmail.com>
2025-12-22 15:48:33 +0300

    clue 4

--- find_carmen.sql
+++ find_carmen.sql
@@ -64,6 +64,22 @@ Result:
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+SELECT name
+FROM city
+WHERE countrycode = (
+    SELECT code
+    FROM country
+    WHERE name = 'Holy See (Vatican City State)'
+)
+AND name <> 'Vatican City';
+
+Result:
+----------------------
+         name
+----------------------
+ Citt∩┐╜ del Vaticano
+(1 row)
+----------------------
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
